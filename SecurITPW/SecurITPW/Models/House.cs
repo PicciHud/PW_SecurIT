e2bd0e9a234cbdae874bf4b42974bee368d623ff
bonale <you@example.com>
2023-06-16 11:56:08 +0200

    added azure_function, worker and fe

---
+++ SecurITPW/SecurITPW/Models/House.cs
@@ -0,0 +1,13 @@
+﻿namespace SecurITPW.Models
+{
+    public class House
+    {
+        public int Id { get; set; }                 // Primary key
+        public int IdHouse { get; set; }
+        public int IdRoom { get; set; }
+        // public ICollection<Access> Accesses { get; set; }
+
+        // La proprietà Accesses è definita come ICollection<Enrollment> perché potrebbero
+        // essere presenti più entità Access correlate
+    }
+}
