6a88b1d8b87b126e04dbdf37dd80af51dd5f125c
alejandro <alessandro.bonaldo@stud.itsaltoadriatico.it>
2023-06-29 23:13:08 +0200

    [FIX] a lot of FE

--- Azure/SecurITPW/SecurITPW/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Azure/SecurITPW/SecurITPW/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -4,11 +4,9 @@
 
 using System;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace SecurITPW.Areas.Identity.Pages.Account
 {
