c78660194603a23bd89af851b63d482c116da535
Alessandro Bonaldo <alessandro.bonaldo@stud.itsaltoadriatico.it>
2023-06-30 17:10:27 +0200

    [FIX] various fixes

--- Azure/SecurITPW/SecurITPW/Models/AspNetUsers.cs
+++ Azure/SecurITPW/SecurITPW/Models/AspNetUsers.cs
@@ -3,10 +3,10 @@
     public class AspNetUsers
     {
         public string? Id { get; set; }
-        public string? UserName { get; set; }
-        public string? NormalizedUserName { get; set; }
+        public string UserName { get; set; }
+        public string NormalizedUserName { get; set; }
         public string? Email { get; set; }
-        public string? NormalizedEmail { get; set; }
+        public string NormalizedEmail { get; set; }
         public bool EmailConfirmed { get; set; }
         public string? PasswordHash { get; set; }
         public string? SecurityStamp { get; set; }
