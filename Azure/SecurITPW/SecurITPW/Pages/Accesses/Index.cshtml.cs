9be796d100384b32664fef9a8aac6b8e868ba7a6
Alessandro Bonaldo <alessandro.bonaldo@stud.itsaltoadriatico.it>
2023-06-30 12:58:57 +0200

    [FIX] i don't remember

--- Azure/SecurITPW/SecurITPW/Pages/Accesses/Index.cshtml.cs
+++ Azure/SecurITPW/SecurITPW/Pages/Accesses/Index.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using SecurITPW.Models;
@@ -15,15 +16,44 @@ namespace SecurITPW.Pages.Accesses
             _context = context;
         }
 
-        public IList<Access> Access { get;set; } = default!;
+        //dichiarato per il bottone
+        public List<Access> Access { get; set; } = default!;
 
         public async Task OnGetAsync()
         {
+            //dichiarato per il bottone
+            //Access = await _context.Access.ToListAsync();
+
             if (_context.Access != null)
             {
                 // Ordina la lista degli accessi in modo decrescente in base all'orario
                 Access = await _context.Access.OrderByDescending(a => a.Time).ToListAsync();
             }
         }
+
+        public async Task<IActionResult> OnPostDeleteAll()
+        {
+            // Crea un'istanza di HttpClient
+            var httpClient = new HttpClient();
+
+            // Effettua la chiamata all'API
+            var response = await httpClient.GetAsync("https://localhost:7061/api/Access");
+
+            if (response.IsSuccessStatusCode)
+            {
+                // Rimuovi eventuali dati nella cache o esegui altre operazioni necessarie dopo la cancellazione della lista
+                // ...
+
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                // Gestisci l'errore in modo appropriato
+                return StatusCode((int)response.StatusCode);
+            }
+        }
     }
 }
+
+
+
